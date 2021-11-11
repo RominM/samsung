1f84328b2131a32f079dc92fb6cc74755b653398
RominM <rominmanogil.off@gmail.com>
2021-11-11 12:12:07 +0100

    head-section is ok but style have to be changed

--- js/view/main/createMain.js
+++ js/view/main/createMain.js
@@ -1,5 +1,7 @@
 export const createMain = () => {
    headSection();
+   mainBtns();
+   aside();
 };
 
 const headSection = () => {
@@ -8,13 +10,13 @@ const headSection = () => {
    const bgHeadSection = document.createElement('div');
    bgHeadSection.classList.add('head-section__bg');
 
-   const bgImg = document.createElement('img');
-   bgImg.classList.add('bg-img');
-   bgImg.src = './../../../images_samsung/neo-top-bg.webp';
+   const bgTop = document.createElement('img');
+   bgTop.classList.add('bg-top');
+   bgTop.src = './../../../images_samsung/neo-top-bg.webp';
 
-   const bgImgAbs = document.createElement('img');
-   bgImgAbs.src = './../../../images_samsung/tv-top.webp';
-   bgImgAbs.classList.add('tv-img')
+   const tvTop = document.createElement('img');
+   tvTop.src = './../../../images_samsung/tv-top.webp';
+   tvTop.classList.add('tv-top')
 
    const p = document.createElement('p');
    p.classList.add('head-section_para');
@@ -28,6 +30,60 @@ const headSection = () => {
 
    p.append(h3, headSectionSpan);
    
-   bgHeadSection.append(bgImg, bgImgAbs);
+   bgHeadSection.append(bgTop, tvTop);
    section.append(bgHeadSection, p);
+}
+
+const mainBtns = () => {
+   const section = document.querySelector('.head-section');
+
+   const mainBtns1 = document.createElement('div');
+   mainBtns1.classList.add('mainBtns1');
+
+   const lightBtn1 = document.createElement('button');
+   lightBtn1.classList.add('light-btn1');
+   lightBtn1.innerHTML = 'Luminosité';
+   const experienceBtn1 = document.createElement('button');
+   experienceBtn1.classList.add('experience-btn1');
+   experienceBtn1.innerHTML = 'Experience';
+   const durabilityBtn1 = document.createElement('button');
+   durabilityBtn1.classList.add('durability-btn1');
+   durabilityBtn1.innerHTML = 'Durabilité';
+
+   mainBtns1.append(lightBtn1, experienceBtn1, durabilityBtn1);
+   section.append(mainBtns1);
+}
+
+const aside = () => {
+   const section = document.querySelector('.head-section');
+
+   const aside = document.createElement('aside');
+
+   const review1 = document.createElement('div');
+   review1.classList.add('review-as1');
+   const review1Img = document.createElement('img');
+   review1Img.src = './../../../images_samsung/review-numeriques.png';
+
+   const review2 = document.createElement('div');
+   review2.classList.add('review-as2');
+   const review2Img = document.createElement('img');
+   review2Img.src = './../../../images_samsung/review-avcesar.webp'
+  
+   const review3 = document.createElement('div');
+   review3.classList.add('review-as3');
+   const review3Img = document.createElement('img');
+   review3Img.src = './../../../images_samsung/review-mustav.webp';
+   
+   const review4 = document.createElement('div');
+   review4.classList.add('review-as4');
+   const review4Img = document.createElement('img');
+   review4Img.src = './../../../images_samsung/review-frandroid.webp';
+
+   review1.append(review1Img);
+   review2.append(review2Img);
+   review3.append(review3Img);
+   review4.append(review4Img);
+
+   aside.append(review1, review2, review3, review4);
+   section.append(aside);
 }
